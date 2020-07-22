d69e00527738051d946a550243692e1f040c5a16
Aatu Piiroinen <aatu.piiroinen@gmail.com>
2020-07-22 13:08:00 +0300

    frontending building lisäys

---
+++ build/precache-manifest.96308862fb2a8bd7a19ebebdd41d1cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6af51fa8d8eb939ce575b37767c50b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "914a4b4826c9f4f740d1",
+    "url": "/static/css/main.7738d310.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914a4b4826c9f4f740d1",
+    "url": "/static/js/main.bd060f24.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
